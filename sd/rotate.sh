c38525e184ebec2b2808b4d5c9a882c045ee0ed9
mtai000 <605836858@qq.com>
2024-10-25 17:02:16 +0800

    dynamic get touch screen device id

--- sd/rotate.sh
+++ sd/rotate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xrandr --output eDP --rotate right
-xinput set-prop 17 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
+device_id=$(xinput list | grep 'FTS3528:00 2808:1015' | grep -v 'UNKNOW' | awk -F'id=' '{print $2}' | awk '{print $1}')
+xinput set-prop $device_id 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
